d2bd50440479fff60d1ff53dbe9655a9c2000354
Stinus <stinus2@gmail.com>
2019-10-11 11:10:35 +0200

    ting

--- TSP.m
+++ TSP.m
@@ -20,64 +20,61 @@ y = y';
 evalPoints = [x; y];
 recordedDistance = calcDist(evalPoints);
 
-% %Make population
-% for i = 1:popCount
-%     population(:,i) = evalPoints(:,i);
-% end
-% population = shuffleFunc(population,100);
+
+population = evalPoints;
+
+%Make population
+for i = 1:popCount
+    [population; shuffleFunc(evalPoints,10)];
+end
+%population = shuffleFunc(population,100);
+
+%Calculate distance and make it the fitness value
+for i = 1:length(population)
+    d = calcDist(population);
+    if (d < recordedDistance)
+        recordedDistance = d;
+        bestEver = population(:,i);
+    end
+    fitness = d;
+end
+
 % 
-% %Calculate distance and make it the fitness value
-% for i = 1:length(population)
-%     d = calcDist(population);
-%     if (d < recordedDistanced)
-%         recordedDistance = d;
-%         bestEver = population(:,i);
+% 
+% 
+% 
+% figure
+% %Plot points
+% for k = 1:length(N)
+%     for l = 1:2
+%         plot(evalPoints(1,:),evalPoints(2,:),'-x');
+%         hold on
 %     end
-%     fitness = d;
+% end
+% axis([0 10 0 10]);
+% 
+% optimizedNumTimes = 0;
+% %Randomly check which candidate solution is the best
+% for i = 1:10000
+%     
+%     evalPoints = shuffleFunc(evalPoints,100);
+%     d = calcDist(evalPoints);
+%     %Plot points only if better d is found
+%     if(d < recordedDistance)
+%         clf;
+%         recordedDistance = d
+%         for k = 1:length(N)
+%             for l = 1:2
+%                 plot(evalPoints(1,:),evalPoints(2,:),'-x k');
+%                 hold on
+%             end
+%         end
+%     axis([0 11 0 11]);
+%     bestEver = evalPoints
+%     optimizedNumTimes = optimizedNumTimes+1;
+%     end
+%     pause(0.001);
 % end
 % 
 % 
 % 
-
-
-
-
-
-
-
-
-figure
-%Plot points
-for k = 1:length(N)
-    for l = 1:2
-        plot(evalPoints(1,:),evalPoints(2,:),'-x');
-        hold on
-    end
-end
-axis([0 10 0 10]);
-
-optimizedNumTimes = 0;
-%Randomly check which candidate solution is the best
-for i = 1:10000
-    
-    evalPoints = shuffleFunc(evalPoints,100);
-    d = calcDist(evalPoints);
-    %Plot points only if better d is found
-    if(d < recordedDistance)
-        clf;
-        recordedDistance = d
-        for k = 1:length(N)
-            for l = 1:2
-                plot(evalPoints(1,:),evalPoints(2,:),'-x k');
-                hold on
-            end
-        end
-    axis([0 11 0 11]);
-    bestEver = evalPoints
-    optimizedNumTimes = optimizedNumTimes+1;
-    end
-    pause(0.001);
-end
-
-
-
